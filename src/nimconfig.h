5619a7f84638ae2792a0562a7901cc2279cb5e03
h2zero <ryan@nable-embedded.io>
2024-12-22 16:17:00 -0700

    Add config to use mbedtls and esp_timer

--- src/nimconfig.h
+++ src/nimconfig.h
@@ -155,6 +155,13 @@
  */
 // #define CONFIG_NIMBLE_CPP_FREERTOS_TASK_BLOCK_BIT 31
 
+/**
+ * @brief Un-comment to use mbedtls instead of tinycrypt.
+ * This could save approximately 8k of flash if already using mbedtls for
+ * other functionality.
+ */
+// #define CONFIG_BT_NIMBLE_CRYPTO_STACK_MBEDTLS 1
+
 /**********************************
  End Arduino user-config
 **********************************/
@@ -281,6 +288,14 @@
 #define CONFIG_BT_CONTROLLER_ENABLED 1
 #endif
 
+#ifndef CONFIG_BT_NIMBLE_CRYPTO_STACK_MBEDTLS
+#define CONFIG_BT_NIMBLE_CRYPTO_STACK_MBEDTLS 0
+#endif
+
+#ifndef MYNEWT_VAL_BLE_CRYPTO_STACK_MBEDTLS
+#define MYNEWT_VAL_BLE_CRYPTO_STACK_MBEDTLS (CONFIG_BT_NIMBLE_CRYPTO_STACK_MBEDTLS)
+#endif
+
 #ifdef ESP_PLATFORM
 #ifndef CONFIG_BTDM_CONTROLLER_MODE_BLE_ONLY
 #define CONFIG_BTDM_CONTROLLER_MODE_BLE_ONLY
@@ -315,6 +330,11 @@
 #    error Extended advertising is not supported on ESP32.
 #  endif
 #endif
+
+#ifndef CONFIG_BT_NIMBLE_USE_ESP_TIMER
+#define CONFIG_BT_NIMBLE_USE_ESP_TIMER 1
+#endif
+
 #endif // ESP_PLATFORM
 
 #if CONFIG_BT_NIMBLE_ENABLE_PERIODIC_ADV && !CONFIG_BT_NIMBLE_EXT_ADV
